cd9b85d85eba7fef118ac602f3f8f1b60adabcb1
Mike Crifo <mike.crifo@gmail.com>
2022-01-23 21:50:48 -0500

    changed axios localhost api url to firebase url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/shopplusplus-2f91d/us-central1/api'
+    baseURL: 'https://us-central1-shopplusplus-2f91d.cloudfunctions.net/api'
 })
 
 export default instance;
+
+// http://localhost:5001/shopplusplus-2f91d/us-central1/api
