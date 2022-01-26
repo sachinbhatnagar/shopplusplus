d8f6d0ab8b6fb592440c2a59df0cf81d4801c909
Mike Crifo <mike.crifo@gmail.com>
2022-01-25 20:17:52 -0500

    push for axios change

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://us-central1-shopplusplus-2f91d.cloudfunctions.net/api'
+    baseURL: 'http://localhost:5001/shopplusplus-2f91d/us-central1/api'
 })
 
 export default instance;
 
 // http://localhost:5001/shopplusplus-2f91d/us-central1/api
+// https://us-central1-shopplusplus-2f91d.cloudfunctions.net/api
