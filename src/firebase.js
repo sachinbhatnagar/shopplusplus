b07aa606e4a533431acbed214ce5b30f1339e9d6
Sachin Bhatnagar <mailme@sachinbhatnagar.com>
2022-03-31 18:27:53 +0530

    Removing firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,9 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCVPWNHJPEoIfdCKzbzU1K0iAjOvA1iqrk",
-    authDomain: "shopplusplus-2f91d.firebaseapp.com",
-    projectId: "shopplusplus-2f91d",
-    storageBucket: "shopplusplus-2f91d.appspot.com",
-    messagingSenderId: "470203064143",
-    appId: "1:470203064143:web:52bb892ff94cda28f0ef3a",
-    measurementId: "G-QB6YTVE387"
-  };
+const firebaseConfig = {};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
-
   const db = firebaseApp.firestore();
   const auth = firebase.auth();
 
