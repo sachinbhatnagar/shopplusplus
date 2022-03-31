69cadd245088a84d6dd222beb3d2b954e3049723
Sachin Bhatnagar <mailme@sachinbhatnagar.com>
2022-03-31 18:37:34 +0530

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -18,7 +18,10 @@ function Login() {
         .then(auth => {
             history.push('/')
         })
-        .catch(error => alert(error.message))
+        .catch(error => {
+          // Create an error display component which can be used to display the error. window.alert() is blocking and must be avoided
+            alert(error.message);
+        })
     }
 
     const register = e => {
@@ -32,7 +35,10 @@ function Login() {
                 history.push('/');
             }
         })
-        .catch(error => alert(error.message));
+        .catch(error => {
+          // Create an error display component which can be used to display the error. window.alert() is blocking and must be avoided
+            alert(error.message);
+        })
     }
 
     return (
@@ -42,16 +48,13 @@ function Login() {
         </Link>
             <div className='login-container'>
                 <h1>Sign-In</h1>
-                <form>
                     <h5>Email</h5>
                     <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
                     <h5>Password</h5>
                     <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
-                    <br></br>
-                    <br></br>
-                    <button type='submit' onClick={signIn} className='login-signin-button'>Sign-In</button>
-                </form>
-
+                    <br />
+                    <br />
+                    <button onClick={signIn} className='login-signin-button'>Sign-In</button>
                 <p> 
                     By signing in, you're agreeing to ShopPlusPLUS's (FAKE) Terms and Conditions of Use and Sale. <br></br>
                     Please see Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
